3dfadf066af5d4f9b1d6f989ccb64be7bca4dc32
webyiyeon <yiyeon79@gmail.com>
2022-06-27 12:51:06 +0900

    DTO
    add

--- src/main/java/model/MovieDTO.java
+++ src/main/java/model/MovieDTO.java
@@ -1,5 +1,82 @@
 package model;
 
 public class MovieDTO {
-
+	private String moviecode;
+	private String title;
+	private String director;
+	private String actors;
+	private String country;
+	private String summary;
+	private String runningtime;
+	private String ratingcode;
+	private java.util.Date opendate;
+	private String image;
+	private String price;
+	public String getMoviecode() {
+		return moviecode;
+	}
+	public void setMoviecode(String moviecode) {
+		this.moviecode = moviecode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getActors() {
+		return actors;
+	}
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getRunningtime() {
+		return runningtime;
+	}
+	public void setRunningtime(String runningtime) {
+		this.runningtime = runningtime;
+	}
+	public String getRatingcode() {
+		return ratingcode;
+	}
+	public void setRatingcode(String ratingcode) {
+		this.ratingcode = ratingcode;
+	}
+	public java.util.Date getOpendate() {
+		return opendate;
+	}
+	public void setOpendate(java.util.Date opendate) {
+		this.opendate = opendate;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
 }
